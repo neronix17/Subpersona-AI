a6eb55997abac3d06d0faf8cbcc15b25f8441422
neronix17 <39186138+neronix17@users.noreply.github.com>
2022-05-03 01:53:11 +0100

    1.0.8141

---
+++ 1.3/Source/SubpersonaAI/Patch_JobDriver_TendPatient_MakeNewToils.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+using HarmonyLib;
+using Verse.AI;
+
+namespace SubpersonaAI
+{
+    [HarmonyPatch(typeof(HealthAIUtility), "ShouldEverReceiveMedicalCareFromPlayer")]
+    public class Patch_HealthAIUtility_ShouldEverReceiveMedicalCareFromPlayer
+    {
+        [HarmonyPostfix]
+        public static bool Prefix(Pawn pawn, bool __result)
+        {
+            if (pawn != null && pawn.def == SubpersonaDefOf.SubAI_SubpersonaShell)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- 1.3/Source/SubpersonaAI/SubpersonaMod.cs
+++ 1.3/Source/SubpersonaAI/SubpersonaMod.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 using Verse;
 
 using HarmonyLib;
+using System.Reflection;
+using System.IO;
 
 namespace SubpersonaAI
 {
@@ -15,11 +17,19 @@ namespace SubpersonaAI
     {
         public SubpersonaMod mod;
 
+        internal static string VersionDir => Path.Combine(ModLister.GetActiveModWithIdentifier("Neronix17.SubpersonaShells").RootDir.FullName, "Version.txt");
+        public static string CurrentVersion { get; private set; }
+
         public SubpersonaMod(ModContentPack content) : base(content)
         {
             this.mod = this;
 
-            Log.Message(":: Subpersona Shells :: 1.0.0 ::");
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            CurrentVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+            Log.Message($":: Subpersona Shells :: {CurrentVersion} ::");
+
+            File.WriteAllText(VersionDir, CurrentVersion);
 
             new Harmony("neronix17.subpersonashells.rimworld").PatchAll();
         }
